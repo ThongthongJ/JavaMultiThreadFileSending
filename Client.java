df282f201288b50f89393121340650d6d947ed4e
SarawinT <contact.sarawin@gmail.com>
2022-08-11 22:35:45 +0700

    Cleaning up

--- Client.java
+++ Client.java
@@ -1,11 +1,4 @@
-
-import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class Client {
