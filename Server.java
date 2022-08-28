0c9c028cfd821834711d10d95842bd1374c8cf8e
SarawinT <61820678+SarawinT@users.noreply.github.com>
2022-08-28 16:25:00 +0700

    Update Server.java

--- Server.java
+++ Server.java
@@ -61,43 +61,21 @@ class ClientHandler extends Thread {
     private Socket s;
     private InputStream is;
     private OutputStream os;
-    private PrintWriter out;
-    private BufferedReader in;
+    private DataOutputStream out;
+    private DataInputStream in;
 
     private final String FILE_STORAGE = "./resources/";
     private final String FILE_1 = "test.txt";
     private final String FILE_2 = "testP.pdf";
-
     private final int THREAD_NUMBER = 10;
+
+    File[] fileList;
 
     ClientHandler(Socket socket, InputStream is, OutputStream os) {
         s = socket;
         this.is = is;
         this.os = os;
-    }
-
-    @Deprecated
-    private boolean sendFileSingle(String fileName) {
-        final int BUFFER_SIZE = 16 * 4096;
-        try {
-            File file = new File(FILE_STORAGE + fileName);
-            InputStream fin = new FileInputStream(file);
-            out.println("Sending File... <file_name>" + "Output" + fileName + "</file_name>" + "<file_size>"
-                    + file.length() + "</file_size>");
-            byte[] bytes = new byte[BUFFER_SIZE];
-            Logger.printLog("Sending " + fileName + " [" + file.length() + " bytes] to " + s.getInetAddress());
-            int count = 0;
-            while ((count = fin.read(bytes)) > 0) {
-                os.write(bytes, 0, count);
-            }
-            fin.close();
-            Logger.printLog(fileName + " sent to " + socketAddress);
-            return true;
-        } catch (Exception e) {
-            Logger.printLog("\n !! Socket Error !! " + e.getMessage());
-            return false;
-        }
-
+        fileList = new File(FILE_STORAGE).listFiles();
     }
 
     private boolean sendFile(String fileName) {
@@ -105,8 +83,7 @@ class ClientHandler extends Thread {
         try {
             File file = new File(FILE_STORAGE + fileName);
             InputStream fin = new FileInputStream(file);
-            out.println("Sending File... <file_name>" + "Output" + fileName + "</file_name>" + "<file_size>"
-                    + file.length() + "</file_size>");
+            out.writeInt((int) file.length());
             Logger.printLog("Sending " + fileName + " [" + file.length() + " bytes] to " + s.getInetAddress());
             byte[] bytes = new byte[(int) file.length()];
 
@@ -140,14 +117,11 @@ class ClientHandler extends Thread {
     }
 
     private String getFileList() {
-        String str = "\n";
-        
 
-        str += " ┌── Select a file to download ──┒\n";
-        str += " │ [1] - " + FILE_1 + "\n";
-        str += " │ [2] - " + FILE_2 + "\n";
-        str += " │ [X] - Exit\n";
-        str += " └───────────────────────────────┚";
+        String str = "";
+        for (File file : fileList) {
+            str += file.getName() + "/";
+        }
 
         return str;
     }
@@ -156,38 +130,23 @@ class ClientHandler extends Thread {
 
         try {
             socketAddress = s.getInetAddress().toString();
-            out = new PrintWriter(os, true);
-            InputStreamReader ir = new InputStreamReader(is);
-            in = new BufferedReader(ir);
+            out = new DataOutputStream(os);
+            in = new DataInputStream(is);
 
             Logger.printLog(socketAddress + " Connected");
-            out.println("Connected to server");
-            out.println(getFileList());
+            out.writeUTF("Connected to server");
+            out.writeUTF(getFileList());
 
-            String inputLine;
+            int index = -1;
 
             // Socket I/O Loop
-            while ((inputLine = in.readLine()) != null) {
+            while ((index = in.readInt()) != -1) {
 
-                if (inputLine.equals("1")) {
-                    sendFile(FILE_1);
-                    out.println(getFileList());
-                } else if (inputLine.equals("2")) {
-                    sendFile(FILE_2);
-                    out.println(getFileList());
+                if (index <= fileList.length) {
+                    sendFile(fileList[index-1].getName());
                 } else {
-                    out.println(getFileList());
-
-                    if (inputLine.equalsIgnoreCase("X")) {
-                        s.close();
-                        break;
-                    }
-
-                    Logger.printLog(s.getInetAddress() + " > " + inputLine);
-                    Thread.sleep(500);
-
+                    Logger.printLog("Invalid file index");
                 }
-
             }
 
         } catch (IOException e) {
@@ -199,9 +158,11 @@ class ClientHandler extends Thread {
                 Logger.printLog("Unhandled Exception > " + e.getMessage());
             }
 
-        } catch (InterruptedException ie) {
-            ie.printStackTrace();
-        } catch (Exception e) {
+        } 
+        // catch (InterruptedException ie) {
+        //     ie.printStackTrace();
+        // }
+         catch (Exception e) {
             Logger.printLog(e);
         }
 
